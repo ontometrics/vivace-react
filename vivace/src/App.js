dd8aa589027f6b161670638a06ec097cfbcac9cc
Masa Sato <masaocsato@yahoo.com>
2018-11-21 12:55:48 -0800

    got basic router working

--- vivace/src/App.js
+++ vivace/src/App.js
@@ -1,29 +1,17 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import Navbar from "./components/Navbar";
 import "./App.css";
 import Catalog from "./components/Catalog";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <Catalog />
+        <Switch>
+          <Route exact path="/Catalog" component={Catalog} />
+        </Switch>
       </div>
     );
   }
